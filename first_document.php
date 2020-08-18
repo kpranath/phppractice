2b2dbff366f23cf70d6672ceda3253a34a83a570
Keshava Pranath K <kpranath@gmail.com>
2020-08-18 23:19:11 +0530

    first doc

---
+++ first_document.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>First php script</title>
+    </head>
+    <body>
+        <?php
+            echo("<p><strong>This is the first document</strong></p>");
+            echo "</br>This is my second php statement";
+        ?>
+    </body>
+</html>
