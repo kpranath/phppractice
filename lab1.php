9635e2f58bf4ecb1e3f5f00080c17893bb0bb435
Keshava Pranath K <kpranath@gmail.com>
2020-08-19 19:31:06 +0530

    lab1 part2

--- lab1.php
+++ lab1.php
@@ -4,7 +4,12 @@
     </head>
     <body>
         <?php
-            echo "<strong><ul><li>Ambassador</li><li>Eterno</li><li>Himalayan</li><li>Scorpio</li></ul><strong>"
+            echo "<strong><ul><li>Ambassador</li><li>Eterno</li><li>Himalayan</li><li>Scorpio</li></ul><strong>";
+            printf("%0.2f",12.99);
+            printf("</br>%0.2f",5.85);
+            printf("</br>%0.2f",100.25);
+            printf("</br>%0.2f",765.25);
+            printf("</br>%0.2f",78.15);
         ?>
     </body>
 </html>
