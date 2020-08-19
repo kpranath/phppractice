f37faf06683e2cd4b11d2daed31afc4c8ab47bf9
Keshava Pranath K <kpranath@gmail.com>
2020-08-19 17:16:48 +0530

    lab1 part1

---
+++ lab1.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Lab1</title>
+    </head>
+    <body>
+        <?php
+            echo "<strong><ul><li>Ambassador</li><li>Eterno</li><li>Himalayan</li><li>Scorpio</li></ul><strong>"
+        ?>
+    </body>
+</html>
