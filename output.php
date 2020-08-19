fb649d3ccff07cb7ec2c5755ffeb4d090c47d5ce
Keshava Pranath K <kpranath@gmail.com>
2020-08-19 15:16:08 +0530

    print and printf

---
+++ output.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Output</title>
+    </head>
+    <body>
+        <?php
+        echo "This is echoed out</br>";
+        print ("This is printed</br>");
+        print "This is also printed, but without()";
+        printf("</br>I am %d years old... this is printed using printf", 22);
+        ?>
+    </body>
+</html>
