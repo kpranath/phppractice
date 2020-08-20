f9d2820dc6de448dfea684e50f9cd5f9bf798783
Keshava Pranath K <kpranath@gmail.com>
2020-08-20 18:49:54 +0530

    lab2 part1

---
+++ lab2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Lab2</title>
+    </head>
+    <body>
+        <?php
+            $operand1 = 15.55;
+            $operand2 = 2.94;
+
+            print($operand1 . " + " . $operand2 . " = " . ($operand1+$operand2) . "</br>");
+            print($operand1 . " - " . $operand2 . " = " . ($operand1-$operand2) . "</br>");
+            print($operand1 . " * " . $operand2 . " = " . ($operand1*$operand2) . "</br>");
+            print($operand1 . " / " . $operand2 . " = " . ($operand1/$operand2) . "</br>");
+            print($operand1 . " % " . $operand2 . " = " . ($operand1%$operand2) . "</br>");
+        ?>
+    </body>
+</html>
