1beefbd72c1a9d04146f291ff468333952dcaa1b
Keshava Pranath K <kpranath@gmail.com>
2020-08-20 09:27:49 +0530

    variable part1

---
+++ variable.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Variable</title>
+    </head>
+    <body>
+        <?php
+            $age = 22;
+            $name = "Pranath";
+
+            print($name);
+            print("</br>");
+            print($age);
+            print("</br>" . $name . " is " . $age . " is years old");
+        ?>
+    </body>
+</html>
